b77825b8b3bfdb1e00de20eee1e3fa9c5920e369
zjuyk <ownbyzjuyk@gmail.com>
2021-09-04 07:33:06 +0800

    Update dot_config/doom/config.el Update dot_config/doom/init.el Update dot_config/private_dolphinrc Update dot_config/private_konsolerc Update dot_config/private_kwinrc Update dot_config/private_mpv/mpv.conf Update dot_config/private_plasma-org.kde.plasma.desktop-appletsrc Update private_dot_mozilla/private_firefox/private_6fcl58u3.default-release/private_prefs.js

--- dot_config/doom/config.el
+++ dot_config/doom/config.el
@@ -19,8 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Fira Code" :size 16 :weight 'semi-light)
-       doom-variable-pitch-font (font-spec :family "sans" :size 16))
+(setq doom-font (font-spec :family "Fira Code" :size 16)
+      doom-variable-pitch-font (font-spec :family "sans-serif" :size 16)
+      doom-unicode-font (font-spec :family "Noto Sans CJK SC" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -34,8 +34,8 @@
        ;;(emoji +unicode)  ; 🙂
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       indent-guides     ; highlighted indent columns
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -43,7 +43,7 @@
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       ;;treemacs          ; a project drawer, like neotree but cooler
+       treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
@@ -74,7 +74,7 @@
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
-       ;;shell             ; simple shell REPL for Emacs
+       shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        ;;vterm             ; the best terminal emulation in Emacs
 
